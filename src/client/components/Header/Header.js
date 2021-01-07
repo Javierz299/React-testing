f5b7be7314b5addde3e79089b1f0fd4dd3aa8c3a
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2021-01-06 20:06:59 -0500

    conditionally rendering sign in and out

--- src/client/components/Header/Header.js
+++ src/client/components/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const state = useSelector(state => state.authReducer.authenticated)
+
+    console.log("state from header",state)
+
+
     return (
         <div>
             <Link to="/" >Redux Auth</Link>
             <Link to="/signup" >Sign up</Link>
-            <Link to="/signin" >Sign in</Link>
+            {state === "" ?
+            <Link to="/signin" >Sign in</Link> :
             <Link to="/signout" >Sign out</Link>
+            }
+            
             <Link to="/feature" >Feature</Link>
         </div>
     )
